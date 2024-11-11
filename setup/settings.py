718a62f519d94c98a4842dcade02d6ce18bf775e
LeoBP1987 <leonardobp1987@gmail.com>
2024-11-11 17:56:48 -0300

    Removendo arquivos estáticos da AWS 3

--- setup/settings.py
+++ setup/settings.py
@@ -112,7 +112,7 @@ AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
 
 AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
 
-AWS_STORAGE_BUCKET_NAME = 'pindorama-api'
+AWS_STORAGE_BUCKET_NAME = 'apindorama'
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
