453dea131ba399f577f1f7a828116b5df6a1b25f
LeoBP1987 <leonardobp1987@gmail.com>
2024-11-11 17:02:41 -0300

    Ajuste de Conexão com AWS

--- setup/settings.py
+++ setup/settings.py
@@ -135,6 +135,8 @@ AWS_HEADER = {
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 
+STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
+
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
