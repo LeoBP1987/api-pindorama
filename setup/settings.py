7aa200c8e917cfc9e3f1f2dd78ecc8a01b6bbfcd
LeoBP1987 <leonardobp1987@gmail.com>
2024-12-09 16:16:12 -0300

    Ajustando liberação de requisição vite

--- setup/settings.py
+++ setup/settings.py
@@ -192,5 +192,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "http://localhost:5173",
-    "http://127.0.0.1:5173/"
+    "http://127.0.0.1:5173/",
+    "http://localhost:4173",
+    "http://127.0.0.1:4173/"
 ]
