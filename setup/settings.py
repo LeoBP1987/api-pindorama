1c011d816650b2d9ad0033a2bd1fce522566840d
LeoBP1987 <leonardobp1987@gmail.com>
2025-05-20 20:38:25 -0300

    Fix: Liberando novo front no cors

--- setup/settings.py
+++ setup/settings.py
@@ -196,7 +196,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:4173",
     "http://127.0.0.1:4173",
-    "https://pindorama-front.vercel.app"
+    "https://pindorama-front.vercel.app",
+    "https://pindorama-kappa.vercel.app/"
 ]
 
 SIMPLE_JWT = {
